d6013d7fdaf3487ff2c79e9b40df67ad0df758e7
zAlle <alexanlima1.0@gmail.com>
2026-07-05 01:12:44 -0300

    feat: criando a barra lateral do comopnent da pagina feed

---
+++ src/app/pages/feed/components/right-sidebar/right-sidebar.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-right-sidebar',
+  imports: [],
+  templateUrl: './right-sidebar.html',
+  styleUrl: './right-sidebar.scss',
+})
+export class RightSidebar {
+  public username: String = "pao_de_casa";
+  public follower: String = "pao_de_casa";
+  public icon: String = "calculate";
+}
